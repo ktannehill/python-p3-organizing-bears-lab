0d3755037100a136f87205c7bee17d1df5c307b8
Kat Tannehill <katetannehill@gmail.com>
2023-10-31 20:03:21 -0600

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'sweet', 1),
+('Rowdy', 13, 'M', 'yellow', 'rowdy', 0),
+('Tabitha', 7, 'F', 'tan', 'nice', 1),
+('Sergeant Brown', 12, 'F', 'brown', 'mean', 0),
+('Melissa', 2, 'F', 'white', 'sweet', 1),
+('Grinch', 9, 'M', 'green', 'mean', 0),
+('Wendy', 4, 'F', 'tan', 'nice', 1),
+(Null, 6, 'M', 'black', 'brave', 1);
